6b5ba1339bf5131e8c7b9935c8a6ccc85e8a5c29
upura <upura0@gmail.com>
2018-01-21 18:48:12 +0900

    Edit for escaping Unicode Error

--- card2vec.py
+++ card2vec.py
@@ -2,9 +2,9 @@
 import json
 import MeCab
 from gensim.models import doc2vec
+from gensim.models.doc2vec import TaggedDocument
 import os
 import random
-
 
 def load_json(target_game_name):
     # カード名とカードテキストの入力データ作成
@@ -30,7 +30,8 @@ def load_json(target_game_name):
                     text += mecab_result
                     texts.append(card["text"])
 
-    with open(target_game_name + ".txt", "w") as file:
+    # Add errors = 'ignore' to escape unicode error in Windows
+    with open(target_game_name + ".txt", "w", errors='ignore') as file:
         file.write(text)
 
     return names, texts
@@ -38,12 +39,32 @@ def load_json(target_game_name):
 
 def generate_doc2vec_model(target_game_name):
     print("Training Start")
-    # カードテキスト読み込み
-    card_text = doc2vec.TaggedLineDocument(target_game_name + ".txt")
-    # 学習
-    model = doc2vec.Doc2Vec(card_text, size=300, window=8, min_count=1,
-                            workers=4, iter=400, dbow_words=1, negative=5)
 
+    # 空のリストを作成（学習データとなる各文書を格納）
+    training_docs = []    
+    f = open(target_game_name + ".txt")
+    lines = f.readlines() # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
+    f.close()
+
+    sent_id = 0
+    sentents = []
+    
+    for line in lines:
+        # 各文書を表すTaggedDocumentクラスのインスタンスを作成
+        sentents.append(line)
+        sent = TaggedDocument(words=line, tags=[sent_id])
+        # 各TaggedDocumentをリストに格納
+        training_docs.append(sent)
+        sent_id += 1
+
+    # 学習実行（パラメータを調整可能）
+    # documents:学習データ（TaggedDocumentのリスト）
+    # min_count=1:最低1回出現した単語を学習に使用する
+    # 学習モデル=DBOW（デフォルトはdm=1:学習モデル=DM）
+    model = doc2vec.Doc2Vec(documents=training_docs, 
+                            size=300, window=8, min_count=1,
+                            workers=4, iter=400,
+                            dbow_words=1, negative=5)
     # モデルの保存
     model.save(target_game_name + ".model")
     print("Training Finish")
@@ -51,7 +72,7 @@ def generate_doc2vec_model(target_game_name):
 
 
 if __name__ == '__main__':
-    TARGET_GAME_NAME = "hearth_stone"
+    TARGET_GAME_NAME = "yugioh"
     names, texts = load_json(TARGET_GAME_NAME)
 
     if os.path.isfile(TARGET_GAME_NAME + ".model") is True:
@@ -60,8 +81,9 @@ if __name__ == '__main__':
         model = generate_doc2vec_model(TARGET_GAME_NAME)
 
     # 類似カードを求めたいカード名
-    TARGET_CARD_NAME = "ロード・ジャラクサス"
-    # TARGET_CARD_NAME = names[random.randint(0, len(names))]
+    TARGET_CARD_NAME = names[random.randint(0, len(names))]
+    # 直接指定も可能
+    # TARGET_CARD_NAME = "xxxxxxxxxxxxxx"
     card_index = names.index(TARGET_CARD_NAME)
 
     # 類似カードと類似度のタプル（類似度上位10件）のリストを受け取る
